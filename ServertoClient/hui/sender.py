c2cdb74e99b192e26ca44d4ed676e5905d49a532
Soundflog <paha-vershinin@mail.ru>
2022-07-01 14:23:25 +0300

    ServertoClient
    Local messager

---
+++ ServertoClient/hui/sender.py
@@ -0,0 +1,9 @@
+import requests
+import response
+
+name=input('Введите имя: ')
+
+while True:
+    text = input("Введите текст: ")
+    response.requests.post(url='http://127.0.0.1:5000/send',
+                           json={'name': name,'text': text})
